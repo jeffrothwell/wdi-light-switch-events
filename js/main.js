0c9532f82913ab17a2d8aa9e59f432bdcb3d39a2
jeffrothwell <rothwelljeff@gmail.com>
2018-02-12 13:42:22 -0500

    toggle button class, body class, modify status contents on button click event

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
+var buttonEl = document.querySelector('.switch');
+var statusEl = document.querySelector('.status');
 
-
+buttonEl.addEventListener('click', function(e){
+  buttonEl.classList.toggle('on');
+  buttonEl.classList.toggle('off');
+  document.querySelector('body').classList.toggle('light');
+  document.querySelector('body').classList.toggle('dark');
+  if (buttonEl.classList.contains('on')) {
+    statusEl.innerText = "It's so briht in here!"
+  } else {
+    statusEl.innerText = "Hey, who turned off the lights?"
+  }
+})
